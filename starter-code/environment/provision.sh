f559c2d84fcbd22d273fd8c16cb2ef2bddb11840
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-18 09:25:49 +0000

    added files to commit

---
+++ starter-code/environment/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install pm2 -g
+cd /app
+sudo npm install
+sudo npm start
