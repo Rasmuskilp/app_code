f7680519f0865ad14bf799b6c97474c00629511d
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-19 09:55:38 +0000

    added changes

--- starter-code/environment/provision.sh
+++ starter-code/environment/provision.sh
@@ -7,5 +7,10 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install pm2 -g
 cd /app
-sudo npm install
-sudo npm start
+#sudo npm install
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+#sudo npm start
